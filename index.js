c9dbb454c3afe093dccf49001644de8fa820e3c7
Shashikant Rupin <shashikantrupin123@gmail.com>
2024-01-23 19:53:51 +0530

    mongodb

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const {router} =require("./routes/aliens")
+
+const app=express();
+
+const mongoUrl = process.env.MONGODB_URL;
+// console.log(mongoUrl);
+
+// const databaseName = "backendcrud";
+
+mongoose.connect(mongoUrl, {
+  // dbName: databaseName,
+  useNewUrlParser: true
+});
+
+const con = mongoose.connection;
+
+con.on('open', () => {
+  console.log("Connected to MongoDB!");
+});
+
+con.on('error', (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+
+app.use(express.json())
+app.use("/aliens", router);
+
+app.listen(9000, ()=>{
+      console.log("listening to the  port 9000...")
+})
